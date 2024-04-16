bbd9e0f3f63d078a6c0a9b7ae26501e1e6cc6923
Ritik Singh <ritik@ritiks-air.mynetworksettings.com>
2024-04-16 02:28:36 -0400

    Updated the user schema

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,6 +1,45 @@
 import mongoose from "mongoose";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+
+
+const preferencesSchema = new mongoose.Schema({
+    userID : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : User
+    },
+    looking_forBHK : {
+        type : Number,
+        required : [true, "Can't be empty!"]
+    },
+    zipcode : {
+        type : Number,
+        required : [true, "Can't be empty!"]
+    },
+    food_choices : {
+        type : String,
+        enum : ["vegetarian", "non-vegetarian"],
+        required : [true, "Can't be empty!"]
+    },
+    profession : {
+        type : String,
+        enum : ["student", "working"],
+        required : [true, "Can't be empty!"]
+    },
+    ages_between : {
+        type : [Number],
+        validate : function(value){
+            return value[0] <= value[1]
+        },
+        message : "Invalid age range",
+    },
+    budget : {
+        type : Number,
+        required : [true, "Budget can't be empty!"]
+    }, 
+    lifestyle_preferences : [{
+        type : [String],
+        enum : ['Drinking', 'Smoking', 'Vegetarian', 'NonVegetarian', 'Pets', 'Cleanliness']
+    }]
+})
 
 const userSchema = new mongoose.Schema({
     username : {
@@ -10,6 +49,7 @@ const userSchema = new mongoose.Schema({
         lowercase : true,
         trim : true,
         index : true,
+        lowercase : true
     },
     email : {
         type : String,
@@ -17,70 +57,44 @@ const userSchema = new mongoose.Schema({
         unique : true,
         trim : true,
     },
-    fullName : {
+    firstName : {
         type : String,
         required : true,
         index : true,
     },
-    avatar : {
-        type: String,
-        required : true,
+    lastName : {
+        type : String,
+        required : true
     },
+    bio : {
+        type : String
+    },
+    age:{
+        type: Number,
+        min: [18, "Age should be greater than 18"],
+        max: 90,
+        required : true
+    },
+    profile_picture : {
+        type: String,
+        required : [true],
+    },
+    likes:[{
+        type : mongoose.Schema.Types.ObjectId,
+        ref: User
+    }],
     coverImage :{
         type: String
     },
-    watchHistory : [{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "Video"
-    }],
     password : {
         type : String,
         required : [true, 'Password is required']
     },
-    refreshToken : {
-        type : String,
-
+    preferences : {
+        type : preferencesSchema,
     }
 }, {timestamps : true});
 
-userSchema.pre("save", async function(next) {
-    if (this.isModified("password")){
-        this.password = await bcrypt.hash(this.password, 4);
-        next();
-    }
-    return next();
-});
-
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password, this.password);
-};
-
-userSchema.methods.generateAccessToken = function(){
-    return jwt.sign(
-        {
-            _id : this._id,
-            email : this.email,
-            fullName : this.fullName,
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn : process.env.ACCESS_TOKEN_EXPIRY,
-        }
-    )
-};
-
-
-userSchema.methods.generateRefreshToken = function(){
-    return jwt.sign(
-        {
-            _id : this._id,
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn : process.env.REFRESH_TOKEN_SECRET_EXPIRY,
-        }
-    )
-};
 
 
 export const User = mongoose.model("User", userSchema);
