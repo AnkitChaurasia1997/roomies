798a5201c2bf2b17e5121161d6015ef2bf6b0e09
Ritik Singh <ritik224@gmail.com>
2024-05-05 15:00:36 -0400

    Update and rename dashboard.controller.js to match.controller.js

--- backend/src/controllers/dashboard.controller.js
+++
@@ -1,6 +0,0 @@
-
-
-export const dashboardController = (req, res) => {
-    const user = JSON.stringify(req.user);
-    return res.render('dashboard', { layout: 'main', userObj : user , username : req.user.username});
-}
---
+++ backend/src/controllers/match.controller.js
@@ -0,0 +1,26 @@
+
+
+export const matchedController = async (req, res) => {
+    try {
+      const user = req.user;
+      const matchedUsersIds = await User.findById(user._id, { matches: 1, _id: 0 });
+      const matchedUsers = await User.find({ _id: { $in: matchedUsersIds.matches } }, {
+        refresh_token: 0,
+        password: 0,
+      });
+      const userObj = JSON.stringify(user);
+
+      return res.status(200).render('matchedUserList', { matchedUsers : matchedUsers, userObj : userObj });
+    //   .json(
+    //     new ApiResponse(
+    //       200,
+    //       {
+    //         likedUsers: likedUsers,
+    //       },
+    //       `All the liked users by ${user.firstName}`
+    //     )
+    //   );
+    } catch (error) {
+      throw new ApiError(500, error?.message || "Error fetching matched users");
+    }
+  };
