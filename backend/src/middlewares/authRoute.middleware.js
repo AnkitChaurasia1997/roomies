2a451ce61bd84dfd34964db0ba969833af62b78e
Ritik Singh <ritik224@gmail.com>
2024-05-06 18:31:31 -0400

    Update authRoute.middleware.js

--- backend/src/middlewares/authRoute.middleware.js
+++ backend/src/middlewares/authRoute.middleware.js
@@ -27,3 +27,20 @@ export const routeCheck = async(req, res, next) => {
         throw new ApiError(401, error?.message || "Invalid access token");
     }
 }
+
+export const ifLogin = async(req, res, next) => {
+    try {
+
+        const token = req.cookies?.accessToken || req.header("Authorization")?.("Bearer", "")
+    
+        
+        if(!token){
+            return res.redirect('/login');
+        }
+
+        
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token");
+    }
+}
